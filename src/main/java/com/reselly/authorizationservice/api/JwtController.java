887ab3dde46987110059ce99c9219c1ac3d13975
Varga Elod <vargaelod23@gmail.com>
2020-12-16 21:31:03 +0200

    Add changes for SQ

--- src/main/java/com/reselly/authorizationservice/api/JwtController.java
+++ src/main/java/com/reselly/authorizationservice/api/JwtController.java
@@ -16,13 +16,39 @@ public class JwtController {
         this.jwtService = jwtService;
     }
 
-    @PostMapping("/")
+    /**
+     * registers the user then issues their jwt
+     * @param credentials the user credentials to persist
+     * @return the user's jwt
+     */
+    @CrossOrigin
+    @PostMapping("/register")
     public String createJwt(@RequestBody Credentials credentials){
+        System.out.println("Created user " + credentials.getEmail());
+        System.out.println("Added something for SQ check");
         return jwtService.generateToken(credentials);
     }
 
+    /**
+     * validates the incoming jwt token against the registered credentials
+     * @param token the jwt token from Headers
+     * @return whether the jwt is valid or not
+     */
+    @CrossOrigin
     @GetMapping("/validate")
-    public boolean validateJwt(@RequestParam("token") String token){
+    public boolean validateJwt(@RequestHeader(value = "Authorization") String token){
         return jwtService.validateToken(token);
+    }
+
+    /**
+     * verified the jwt, if present, then authenticates the user
+     * @param token the possible auth-token
+     * @param credentials the user credentials
+     * @return the re-issued jwt
+     */
+    @CrossOrigin
+    @PostMapping("/login")
+    public String loginAndReturnJwt(@RequestHeader(value = "Authorization") String token, @RequestBody Credentials credentials){
+        return jwtService.loginUser(token, credentials);
     }
 }
